e99703453b63fd9b8f1369fbe4cafa1691b18fb0
mohoshin47 <mmohoshin47@gmail.comm>
2026-06-28 11:32:24 +0600

    Update project

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,43 @@
-import { useState } from "react";
-import Navbar from "./components/Navbar";
-import { Toaster } from "react-hot-toast";
-import Earn from "./pages/Earn";
-import Create from "./pages/Create";
-import Referral from "./pages/Referral";
-import Withdraw from "./pages/Withdraw";
-import Profile from "./pages/Profile";
+import Navbar from './components/Navbar';
+import { Toaster } from 'react-hot-toast';
+import Earn from './pages/Earn';
+import Create from './pages/Create';
+import Referral from './pages/Referral';
+import Withdraw from './pages/Withdraw';
+import Profile from './pages/Profile';
+import { useEffect, useState } from 'react';
 
 export default function App() {
-  const [activeTab, setActiveTab] = useState("earn");
+  const [activeTab, setActiveTab] = useState('earn');
+  const [refreshKey, setRefreshKey] = useState(0);
+
+  useEffect(() => {
+    const handleVisible = () => {
+      if (document.visibilityState === 'visible') {
+        console.log('App Resume');
+
+        setRefreshKey((prev) => prev + 1);
+      }
+    };
+
+    document.addEventListener('visibilitychange', handleVisible);
+
+    return () => {
+      document.removeEventListener('visibilitychange', handleVisible);
+    };
+  }, []);
 
   const renderPage = () => {
     switch (activeTab) {
-      case "earn":
+      case 'earn':
         return <Earn />;
-      case "create":
+      case 'create':
         return <Create />;
-      case "referral":
+      case 'referral':
         return <Referral />;
-      case "withdraw":
+      case 'withdraw':
         return <Withdraw />;
-      case "profile":
+      case 'profile':
         return <Profile />;
       default:
         return <Earn />;
@@ -29,13 +46,10 @@ export default function App() {
 
   return (
     <div className="bg-[#050B17] min-h-screen text-white">
-       <Toaster position="top-center" />
+      <Toaster position="top-center" />
       {renderPage()}
 
-      <Navbar
-        activeTab={activeTab}
-        setActiveTab={setActiveTab}
-      />
+      <Navbar activeTab={activeTab} setActiveTab={setActiveTab} />
     </div>
   );
 }
